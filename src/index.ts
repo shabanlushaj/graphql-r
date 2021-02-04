035c6a7e6305097d3202953ceef827be962183d5
banti <bantlushaj@gmail.com>
2021-02-04 08:54:53 +0100

    GraphQL schema with Apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { listings } from './listings'
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
 
-const app = express()
-const port = 9000
+const app = express();
+const port = 9000;
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.use(bodyParser.json())
+server.applyMiddleware({ app, path: '/api' });
+app.listen(port);
 
-
-app.get('/listings',(_req,res)=>{
-    return res.send(listings)
-})
-
-app.post('/delete-listing',(req,res)=>{
-    const id: string= req.body.id
-    for (let i = 0; i < listings.length; i++) {
-        {
-            if(listings[i].id===id){
-                return res.send(listings.splice(i,1))
-            }
-        }
-    }
-    return res.send('Failed to delete')
-})
-
-app.listen(port)
-
-console.log(`http://localhost:${port}`)
+console.log(`[app] : http://localhost:${port}`);
