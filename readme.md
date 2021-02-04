b3a7db862052621b98a90d0e85c09698a2521b85
banti <bantlushaj@gmail.com>
2021-02-04 06:31:30 +0100

    Express with ts set-up

---
+++ readme.md
@@ -0,0 +1,16 @@
+# REST API  
+
+## Scripts  
+
+Install dependencies :
+`npm i`
+
+To start the express server :
+
+`npm run start`
+
+Build for production :  
+
+`npm run build`
+
+&nbsp;
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 9000
+
+app.get('/',(_req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(port)
+
+console.log(`http://localhost:${port}`)
